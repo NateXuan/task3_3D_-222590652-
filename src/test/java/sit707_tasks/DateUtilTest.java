bd1308f1ea0fc9c4d3bfcf48803c79a18080655a
NateXuan <natexuan0805@gmail.com>
2024-03-01 00:20:52 +1100

    Task 3.2C

--- src/test/java/sit707_tasks/DateUtilTest.java
+++ src/test/java/sit707_tasks/DateUtilTest.java
@@ -5,10 +5,6 @@ import java.util.Random;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-/**
- * @author Ahsan Habib
- */
 public class DateUtilTest {
 	
 	@Test
@@ -103,4 +99,60 @@ public class DateUtilTest {
 	    Assert.assertEquals(2, date.getMonth());
 	    Assert.assertEquals(29, date.getDay());
 	}
+	
+	@Test
+	public void testInvalidYearLowerBoundary() {
+	    new DateUtil(1, 1, 1699); 
+	}
+
+	@Test
+	public void testInvalidYearUpperBoundary() {
+	    new DateUtil(31, 12, 2025); 
+	}
+
+	@Test
+	public void testFebruaryNonLeapYear() {
+	    new DateUtil(29, 2, 2023); 
+	}
+	
+	//Apr, Jun, Sep, Nov have date 30.
+	@Test
+    public void test30DaysMonthsAprilJuneSeptemberNovember30() {
+        DateUtil dateApril = new DateUtil(30, 4, 2024);
+        Assert.assertEquals("30 April 2024", dateApril.toString());
+        DateUtil dateJune = new DateUtil(30, 6, 2024);
+        Assert.assertEquals("30 June 2024", dateJune.toString());
+        DateUtil dateSeptember = new DateUtil(30, 9, 2024);
+        Assert.assertEquals("30 September 2024", dateSeptember.toString());
+        DateUtil dateNovember = new DateUtil(30, 11, 2024);
+        Assert.assertEquals("30 November 2024", dateNovember.toString());
+    }
+	
+	//Apr, Jun, Sep, Nov haven't date 31.
+	@Test
+    public void test30DaysMonthsAprilJuneSeptemberNovember31() {
+        new DateUtil(31, 4, 2024);
+        new DateUtil(31, 6, 2024);
+        new DateUtil(31, 9, 2024);
+        new DateUtil(31, 11, 2024);
+    }
+	
+	//Jan, Mar, May, Jul, Aug, Oct, Dec have data 31.
+	@Test
+    public void test31DaysMonthsJanuaryMarchMayJulyAugustOctoberDecember31() {
+        DateUtil dateJanuary = new DateUtil(31, 1, 2024);
+        Assert.assertEquals("31 January 2024", dateJanuary.toString());
+        DateUtil dateMarch = new DateUtil(31, 3, 2024);
+        Assert.assertEquals("31 March 2024", dateMarch.toString());
+        DateUtil dateMay = new DateUtil(31, 5, 2024);
+        Assert.assertEquals("31 May 2024", dateMay.toString());
+        DateUtil dateJuly = new DateUtil(31, 7, 2024);
+        Assert.assertEquals("31 July 2024", dateJuly.toString());
+        DateUtil dateAugust = new DateUtil(31, 8, 2024);
+        Assert.assertEquals("31 August 2024", dateAugust.toString());
+        DateUtil dateOctober = new DateUtil(31, 10, 2024);
+        Assert.assertEquals("31 October 2024", dateOctober.toString());
+        DateUtil dateDecember = new DateUtil(31, 12, 2024);
+        Assert.assertEquals("31 December 2024", dateDecember.toString());
+    }
 }
